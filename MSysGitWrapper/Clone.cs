b6429392ed3ebf30098b07475a5b4fe089cb29eb
Wilbert van Dolleweerd <wilbert@arentheym.com>
2011-09-08 11:26:03 +0200

    Implement GenerateFullPathToTool method
    Use the registry to search for an installed version of msysgit.

--- MSysGitWrapper/Clone.cs
+++ MSysGitWrapper/Clone.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using Microsoft.Build.Utilities;
 using Microsoft.Build.Framework;
+using Microsoft.Win32;
 
 namespace MsysGit
 {
@@ -58,7 +60,34 @@ namespace MsysGit
         /// </returns>
         protected override string GenerateFullPathToTool()
         {
-            throw new NotImplementedException();
+            // Use the registry to search for the uninstall information of the msysgit installer.
+            string regKey = @"software\microsoft\windows\currentversion\uninstall\git_is1";
+
+            using (RegistryKey key = Registry.LocalMachine.OpenSubKey(regKey))
+            {
+                if (key != null)
+                {
+                    string version = key.GetValue ("DisplayName").ToString();
+                    if (version != null)
+                    {
+                        Log.LogMessage(MessageImportance.Low, string.Format("Found an installed msysgit. Version: {0}", version));
+                    }
+
+                    string partialPath = key.GetValue("InstallLocation").ToString();
+                    if (version == null)
+                    {
+                        Log.LogError(string.Format ("Cannot read the value 'InstallLocation' using the registry key: HKEY_LOCAL_MACHINE\\{0}", regKey));
+                        return null;
+                    }
+                
+                    return Path.Combine (partialPath, "bin", ToolName);
+                }
+                else
+                {
+                    Log.LogError(String.Format("Cannot locate the install location of msysgit using the registry key: HKEY_LOCAL_MACHINE\\{0}", regKey));
+                    return null;
+                }
+            }
         }
 
         /// <summary>
@@ -67,7 +96,7 @@ namespace MsysGit
         /// <returns>The name of the executable file to run.</returns>
         protected override string ToolName
         {
-            get { throw new NotImplementedException(); }
+            get { return "git.exe"; }
         }
 
 
