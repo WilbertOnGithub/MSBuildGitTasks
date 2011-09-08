2b4f756dc3adaf4a9ebc8f494f72860f30814463
Wilbert van Dolleweerd <wilbert@arentheym.com>
2011-09-08 13:07:15 +0200

    Added default override methods for ToolTask

--- MSysGitWrapper/Checkout.cs
+++ MSysGitWrapper/Checkout.cs
@@ -2,10 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
 
 namespace MsysGitWrapper
 {
-    class Checkout
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Checkout : ToolTask
     {
+        protected override string GenerateFullPathToTool()
+        {
+            return string.Empty;
+        }
+
+        protected override string ToolName
+        {
+            get { return "git.exe"; }
+        }
+
+        protected override string GenerateCommandLineCommands()
+        {
+            return string.Empty;
+        }
     }
 }
