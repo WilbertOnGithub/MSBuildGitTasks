0e4ae4861e259bc3d04bbd006407237ba1a55d6d
Wilbert van Dolleweerd <wilbert@arentheym.com>
2011-09-08 15:06:48 +0200

    First implementation of HEAD functionality
    Head task returns the SHA of a given local Git repository. This
    is necessary if you want to store against which SHA you've made
    a certain build.

---
+++ MSysGitWrapper/Head.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace MsysgitWrapper
+{
+    /// <summary>
+    /// MSBuild task that mimics the 'git rev-parse' functionality.
+    /// Given a specific local repository, this task will return the SHA of the head. 
+    /// This is necessary if you want to include against what SHA you have built.
+    /// </summary>
+    public class Head : ToolTask
+    {
+        private string _SHA;
+
+        [Required]
+        public string LocalRepository { get; set; }
+
+        [Output]
+        public string SHA
+        {
+            get { return _SHA; }
+        }
+
+        /// <summary>
+        /// Returns the fully qualified path to the executable file.
+        /// </summary>
+        /// <returns>
+        /// The fully qualified path to the executable file.
+        /// </returns>
+        protected override string GenerateFullPathToTool()
+        {
+            return MsysgitWrapper.Locator.GenerateFullPathToTool(Log);
+        }
+
+        /// <summary>
+        /// Gets the name of the executable file to run.
+        /// </summary>
+        /// <returns>The name of the executable file to run.</returns>
+        protected override string ToolName
+        {
+            get { return MsysgitWrapper.Locator.ToolName; }
+        }
+
+        /// <summary>
+        /// Returns the directory in which to run the executable file.
+        /// </summary>
+        /// <returns>
+        /// The directory in which to run the executable file, or a null reference if the executable file should be run in the current directory.
+        /// </returns>
+        protected override string GetWorkingDirectory()
+        {
+            // Because the log command has to work on the local repository, we have to change the working directory
+            // to the directory where the local repository is stored.
+            return LocalRepository;
+        }
+
+        /// <summary>
+        /// Parses a single line of text to identify any errors or warnings in canonical format.
+        /// </summary>
+        /// <param name="singleLine">A single line of text for the method to parse.</param>
+        /// <param name="messageImportance">A value of <see cref="T:Microsoft.Build.Framework.MessageImportance"/> that indicates the importance level with which to log the message.</param>
+        protected override void LogEventsFromTextOutput(string singleLine, MessageImportance messageImportance)
+        {
+            //Regular expression to catch a SHA.
+
+            base.LogEventsFromTextOutput(singleLine, messageImportance);
+        }
+
+        /// <summary>
+        /// Returns a string value containing the command line arguments to pass directly to the executable file.
+        /// </summary>
+        /// <returns>
+        /// A string value containing the command line arguments to pass directly to the executable file.
+        /// </returns>
+        protected override string GenerateCommandLineCommands()
+        {
+            CommandLineBuilder builder = new CommandLineBuilder();
+            builder.AppendSwitch("rev-parse");
+            builder.AppendSwitch("HEAD");
+
+            return builder.ToString();
+        }
+    }
+}
