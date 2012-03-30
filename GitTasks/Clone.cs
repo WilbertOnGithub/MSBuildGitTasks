b5cf93e1ddb2be141d15395812461465eed7fb6a
Wilbert van Dolleweerd <wilbert@arentheym.com>
2012-03-30 14:21:12 +0200

    Complete first implementation with NGit

--- GitTasks/Clone.cs
+++ GitTasks/Clone.cs
@@ -4,14 +4,50 @@ using System.Linq;
 using System.Text;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
+using NGit;
+using NGit.Api;
+using Sharpen;
 
 namespace GitTasks
 {
 	public class Clone : Task
 	{
+		/// <summary>
+		/// Gets or sets the repository to clone.
+		/// </summary>
+		/// <value>
+		/// The repository to clone.
+		/// </value>
+		[Required]
+		public string RepositoryToClone { get; set; }
+
+		/// <summary>
+		/// Gets or sets the target directory.
+		/// </summary>
+		/// <value>
+		/// The target directory.
+		/// </value>
+		[Required]
+		public string TargetDirectory { get; set; }
+
 		public override bool Execute()
 		{
-			throw new NotImplementedException();
+			try
+			{
+				Git repo = Git.CloneRepository().
+					SetURI(RepositoryToClone).
+					SetDirectory(new FilePath(TargetDirectory)).
+					SetBare(false).
+					SetCloneAllBranches(true).
+					Call();
+			}
+			catch (Exception ex)
+			{
+				Log.LogErrorFromException(ex);
+				return false;
+			}
+
+			return true;
 		}
 	}
 }
