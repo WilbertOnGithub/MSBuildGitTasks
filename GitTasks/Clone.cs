390886749a68e6952f386d7e1f926b459b62a346
Wilbert van Dolleweerd <wilbert@arentheym.com>
2012-03-30 14:13:36 +0200

    Add reference to MSBuild

--- GitTasks/Clone.cs
+++ GitTasks/Clone.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
 
 namespace GitTasks
 {
-	public class Clone
+	public class Clone : Task
 	{
+		public override bool Execute()
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
