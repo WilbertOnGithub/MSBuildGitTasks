b8e74b9fa3db31aa96dcee342e63d07e29e0fd7c
Wilbert van Dolleweerd <wilbert@arentheym.com>
2012-04-10 14:17:39 +0200

    Add Git Facade interface

---
+++ GitTasks/IGitFacade.cs
@@ -0,0 +1,12 @@
+﻿namespace GitTasks
+{
+	/// <summary>
+	/// Facade for NGit
+	/// </summary>
+	public interface IGitFacade
+	{
+		void Clone(string repositoryToClone, string targetDirectory);
+		void CheckoutBranch(string localRepository, string branch);
+		string GetLatestSHA(string localRepository);
+	}
+}
