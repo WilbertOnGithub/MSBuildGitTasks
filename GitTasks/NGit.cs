31e0937aa56f130e74b06a11d765249f29162966
Wilbert van Dolleweerd <wilbert@arentheym.com>
2012-04-10 18:12:59 +0200

    Add GPL3 license

--- GitTasks/NGit.cs
+++ GitTasks/NGit.cs
@@ -1,4 +1,22 @@
-﻿using NGit;
+﻿/*
+ * Copyright 2011 Wilbert van Dolleweerd (wilbert@arentheym.com)
+ * This file is part of the MSBuildGitTasks project
+ *
+ * MsysgitWrapper is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MsysgitWrapper is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MSBuildGitTasks.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using NGit;
 using NGit.Api;
 using Sharpen;
 
